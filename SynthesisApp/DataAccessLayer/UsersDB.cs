ff5d4d67337a41e27b075f867be94a136f21e0f9
Martin Todorov <i481796@fhict.nl>
2022-05-31 16:16:11 +0200

    Minor project changes and some documentation changes

--- SynthesisApp/DataAccessLayer/UsersDB.cs
+++ SynthesisApp/DataAccessLayer/UsersDB.cs
@@ -98,7 +98,7 @@ namespace DataAccessLayer
             }
             catch(Exception ex)
             {
-
+                throw;
             }
             finally
             {
--- SynthesisApp/Entities/Tournament.cs
+++ SynthesisApp/Entities/Tournament.cs
@@ -11,18 +11,21 @@ namespace Entities
         private int id;
         private List<User> playersInTournament;
         private List<Match> matches;
-        TournamentType tournamentType;
-        string tournamentInfo;
-        DateTime startDate;
-        DateTime endDate;
-        int minPlayers;
-        int maxPlayers;
-        string location;
+        private TournamentType tournamentType;
+        private string tournamentName;
+        private string tournamentInfo;
+        private DateTime startDate;
+        private DateTime endDate;
+        private int minPlayers;
+        private int maxPlayers;
+        private string location;
+        private string tournamentStatus;
 
         public int Id { get { return id; } }
         public List<User> PlayersInTournament { get { return playersInTournament; } }
         public List<Match> Match { get { return matches; } }
         public TournamentType TournamentType { get { return tournamentType; } }
+        public string TournamentName { get { return tournamentName; } }
         public string TournamentInfo { get { return tournamentInfo; } }
         public DateTime StartDate { get { return startDate; }
             private set
@@ -45,6 +48,7 @@ namespace Entities
 
         public int MaxPlayers { get { return maxPlayers; } }
         public string Location { get { return location; } }
+        public string TournamentStatus { get { return tournamentStatus; } }
 
         public void checkDate(DateTime startDate, DateTime endDate)
         {
